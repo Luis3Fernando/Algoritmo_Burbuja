4580168193adf03497242ae2431924ea9435c8de
Luis Fernando <luisfernando3chr@gmail.com>
2022-08-08 11:42:58 -0500

    primer commit

---
+++ Metodo_Burbuja.py
@@ -0,0 +1,16 @@
+def Main():
+    lista=[]
+    
+    n = int(input("Digite el numero de elementos para la lista: "))
+    
+    for i in range(n):
+        lista.append(int(input("Digite un elemento para la lista: ")))
+    
+    print("\nELEMENTOS DE LA LISTA: ")
+    for i in lista:
+        print(i)
+        
+    #print("METODO DE ORDENAMIENTO BURBUJA")
+
+if __name__ =='__main__':
+    Main()
